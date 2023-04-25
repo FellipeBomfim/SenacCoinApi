75c7d945041e3af55abc69ee41ada1f90409563c
Feplyss <99778502+Feplyss@users.noreply.github.com>
2023-04-25 20:13:33 -0300

    Update SenacCoin.java

--- src/main/java/br/com/senac/domain/SenacCoin.java
+++ src/main/java/br/com/senac/domain/SenacCoin.java
@@ -1,5 +1,6 @@
 package br.com.senac.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,8 +18,12 @@ import lombok.NoArgsConstructor;
 public class SenacCoin {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "senac_coin_id", nullable = false)
 	private Long id;
-	private int saldo;
-	private String status;
+	@Column(name = "senac_coin_saldo", nullable = false)
+	private Long saldo;
+	@Column(name = "senac_coin_status", nullable = false)
+	private int status;
+	@Column(name = "usuario_id", nullable = false)
 	private String usuarioId;
 }
