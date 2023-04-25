895b4b02966b30bbd1fac62a656a1d276bc7343d
Feplyss <99778502+Feplyss@users.noreply.github.com>
2023-04-25 20:13:04 -0300

    Update SenacCoinMovimentacao.java

--- src/main/java/br/com/senac/domain/SenacCoinMovimentacao.java
+++ src/main/java/br/com/senac/domain/SenacCoinMovimentacao.java
@@ -1,7 +1,9 @@
 package br.com.senac.domain;
 
-import java.time.LocalDate;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,11 +21,18 @@ import lombok.NoArgsConstructor;
 public class SenacCoinMovimentacao {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "senac_coin_movimentacao_id", nullable = false)
 	private Long id;
-	private LocalDate data;
+	@Column(name = "senac_coin_movimentacao_data", nullable = false)
+	private Date data;
+	@Column(name = "senac_coin_movimentacao_observacao", columnDefinition = "LONGTEXT", nullable = false)
 	private String observacao;
-	private int valor;
+	@Column(name = "senac_coin_movimentacao_valor", nullable = false)
+	private Long valor;
+	@Column(name = "senac_coin_movimentacao_status", nullable = false)
 	private int status;
+	@Column(name = "senac_coin_id", nullable = false)
 	private Long senacCoinId;
+	@Column(name = "usuario_id", nullable = false)
 	private String usuarioId;
 }
